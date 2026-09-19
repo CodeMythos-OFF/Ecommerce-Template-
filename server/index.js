901edb9ebd6ce3bef618e7b756ccf928730c82e6
CODEMYTHOS <codemythos@outlook.com>
2026-09-19 17:04:15 +0530

    Fix CORS origin URL by removing trailing slash

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors({
     'http://localhost:5173',
     'http://localhost:3000',
     'https://scs577738.vercel.app',
-    'https://testsampleindev.vercel.app/'
+    'https://testsampleindev.vercel.app'
   ],
   credentials: true
 }));
